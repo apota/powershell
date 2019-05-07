63c23839e05f926cd69b109ce62b311645becb21
Alok Pota <alok.pota@coxautoinc.com>
2019-05-07 15:17:30 -0500

    Fixing regex

--- reverse_phone_lookup/phone_lookup.ps1
+++ reverse_phone_lookup/phone_lookup.ps1
@@ -1,20 +1,21 @@
 ﻿$phone = $args[0]
 $phone -match '(?<first3>\d\d\d)(-)?(?<mid3>\d\d\d)(-)?(?<last4>\d\d\d\d)'  #making the phone delimiter - optional
 
+#Normalize the phone number back to having -
+$phone = "{0}-{1}-{2}" -f $Matches.first3, $Matches.mid3, $Matches.last4
+
 $surl= "https://theylookup.com/{0}-{1}.html" -f $Matches.first3, $Matches.mid3
-
 $anchorNextId = "{0}-{1}_next" -f $Matches.first3, $Matches.mid3
-
 $pattern = '<td class=" ">{0}</td><td class=" ">(?<phoneOwner>(.)+)</td>' -f $phone
 
+#invoke IE
 $ie = new-object -com "InternetExplorer.Application"
 $ie.visible = $false
 $ie.navigate($surl)
-
 do {sleep 1} until (-not ($ie.Busy))
 
+#Play w/ the dom
 $doc = $ie.Document;
-
 
 while($doc -ne $null) {
  
